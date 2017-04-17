fedd995d119b58e48fed019590efb76bf387f29b
zangshengbo <zangshengbo18@gmail.com>
2017-04-17 21:11:42 +0100

    Update distance.py

--- distance.py
+++ distance.py
@@ -3,9 +3,12 @@ import time
 
 Trig_Pin = 20
 Echo_Pin = 21
+Led_Pin = 16
+maximum_dis = 80
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Trig_Pin, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(Led_Pin, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup(Echo_Pin, GPIO.IN)
 
 time.sleep(2)
@@ -22,9 +25,16 @@ def checkdist():
     t2 = time.time()
     return (t2-t1)*340*100/2
 
+
 try:
     while True:
-        print 'Distance:%0.2f cm' % checkdist()
+        if(checkdist() > maximum_dis)
+           {
+               print 'warning'
+               GPIO.output(Led_Pin, GPIO.HIGH)
+           }
+        else
+           print 'Distance:%0.2f cm' % checkdist()
         time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
